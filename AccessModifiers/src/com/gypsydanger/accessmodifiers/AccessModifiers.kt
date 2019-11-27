29876c90d1dacb34b0e73306912d9caa55a772a8
padovese <bhspadoveze@gmail.com>
2019-11-26 21:08:36 -0300

    chore: private classes

---
+++ AccessModifiers/src/com/gypsydanger/accessmodifiers/AccessModifiers.kt
@@ -0,0 +1,9 @@
+package com.gypsydanger.accessmodifiers
+
+fun main(args: Array<String>) {
+    val emp = Employee()
+}
+
+private class Employee(){
+    //in Kotlin, classes could be private, protected or module(Idea IDE module). And of course public.
+}
