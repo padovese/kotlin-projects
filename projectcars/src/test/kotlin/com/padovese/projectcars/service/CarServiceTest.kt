fae458d2f5e7f2930a24e6f2538f212b2cc65fde
padovese <bpadovese@uolinc.com>
2020-10-11 19:46:46 -0300

    refactor: unused imports

--- projectcars/src/test/kotlin/com/padovese/projectcars/service/CarServiceTest.kt
+++ projectcars/src/test/kotlin/com/padovese/projectcars/service/CarServiceTest.kt
@@ -1,15 +1,10 @@
 package com.padovese.projectcars.service
 
-import com.padovese.projectcars.document.Car
-import com.padovese.projectcars.repository.CarRepository
 import com.padovese.projectcars.utils.LoadData
-import org.junit.jupiter.api.AfterEach
-import org.junit.jupiter.api.BeforeEach
 import org.junit.jupiter.api.Test
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.boot.test.context.SpringBootTest
 import reactor.kotlin.test.test
-import java.util.*
 
 @SpringBootTest
 class CarServiceTest : LoadData() {
