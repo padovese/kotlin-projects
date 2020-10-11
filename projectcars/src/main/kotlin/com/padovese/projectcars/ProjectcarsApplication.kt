a84e7b5294c28d4959957631f72fb2ca0e5dc020
padovese <bpadovese@uolinc.com>
2020-10-11 19:43:00 -0300

    chore: initial commit

---
+++ projectcars/src/main/kotlin/com/padovese/projectcars/ProjectcarsApplication.kt
@@ -0,0 +1,11 @@
+package com.padovese.projectcars
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class ProjectcarsApplication
+
+fun main(args: Array<String>) {
+	runApplication<ProjectcarsApplication>(*args)
+}
---
+++ projectcars/src/test/kotlin/com/padovese/projectcars/ProjectcarsApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.padovese.projectcars
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class ProjectcarsApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
